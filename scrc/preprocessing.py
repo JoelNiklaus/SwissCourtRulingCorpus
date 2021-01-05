c719f50dc7dd65e8ca96fd4600b7410211af483e
Joel Niklaus <me@joelniklaus.ch>
2021-01-05 15:55:08 +0200

    added support for paragraphs and sections

--- scrc/preprocessing.py
+++ scrc/preprocessing.py
@@ -18,28 +18,39 @@ pd.set_option('display.width', 1000)
 
 DATA_PATH = 'data/bger/de/html'
 
+# As soon as one of the strings in the list (value) is encountered we switch to the corresponding section (key)
+section_markers = {
+    "judges": ['Besetzung'],
+    "parties": ['Parteien'],
+    "topic": ['Gegenstand'],
+    "situation": ['Sachverhalt'],
+    "considerations": ['Das Bundesgericht zieht in Erwägung', 'Erwägungen'],
+    "rulings": ['Demnach erkennt das Bundesgericht'],
+}
+
 
 def handle_bger():
-    data = {'language': [], 'court': [], 'court_id': [], 'file_type': [], 'file_name': [], 'title': [], 'text': [], }
+    data = {'language': [], 'court': [], 'court_id': [], 'file_type': [], 'file_name': [],  # meta information
+            'title': [], 'raw_text': [], 'paragraphs': [], }  # content
+
+    file_name = 'U_99-2006.html'
 
     data['language'].append('de')
     data['court'].append('Bundesgericht')
     data['court_id'].append('bger')
     data['file_type'].append('html')
-    file_name = 'U_99-2006.html'
+    data['file_name'].append(file_name)
+
     with open(file_name) as f:
         html = f.read()
-
     soup = bs4.BeautifulSoup(html, "html.parser")  # parse html
-    data['file_name'].append(file_name)
+    divs = soup.find_all("div", class_="content")
+    assert len(divs) == 2  # we only expect two divs with class text
+
     data['title'].append(soup.title.text)  # get the title
-    texts = soup.find_all("div", class_="content")
-    assert len(texts) == 2  # we only expect two divs with class text
-    text = texts[0].get_text()  # the first one contains the decision, the second one the navigation
-    text = re.sub(r"\u00a0", ' ', text)  # remove NBSP
-    text = re.sub(r"\s+", ' ', text)  # remove all new lines
-    text = re.sub(r"_+", '_', text)  # remove duplicate underscores (from anonymisations)
-    data['text'].append(text)
+    data['raw_text'].append(get_raw_text(divs[0]))
+    paragraphs = get_paragraphs(divs[0])
+    data['paragraphs'].append(paragraphs)
 
     df = pd.DataFrame.from_dict(data)
     print(df)
@@ -47,19 +58,84 @@ def handle_bger():
     return df
 
 
+def get_raw_text(html):
+    """
+    Add the entire text: harder for doing sentence splitting later because of header and footer
+    :param html:
+    :return:
+    """
+    raw_text = html.get_text()  # the first one contains the decision, the second one the navigation
+    raw_text = clean_text(raw_text)
+    return raw_text
+
+
+def get_paragraphs(html):
+    """
+    Get Paragraphs in the decision
+    :param html:
+    :return:
+    """
+    paragraphs = []
+    for div in html:
+        if isinstance(div, bs4.element.Tag):
+            text = str(div.string)
+            # This is a hack to also get tags which contain other tags such as links to BGEs
+            if text.strip() == 'None':
+                text = div.get_text()
+            paragraph = clean_text(text)
+            if paragraph not in ['', ' ',]:  # Only keep information
+                paragraphs.append(paragraph)
+    return paragraphs
+
+
+def clean_text(text):
+    """
+    Clean text from nasty tokens
+    :param text:
+    :return:
+    """
+    text = re.sub(r"\u00a0", ' ', text)  # remove NBSP
+    text = re.sub(r"\s+", ' ', text)  # remove all new lines
+    text = re.sub(r"_+", '_', text)  # remove duplicate underscores (from anonymisations)
+    text = text.strip()  # remove leading and trailing whitespace
+    return text
+
+
 def bring_to_prodigy_format(df):
     # make sure it is downloaded with python -m spacy download de_core_news_md
-    nlp = spacy.load("de_core_news_md", disable=["tagger", "textcat", "ner"], n_process=-1)  # only keep parser
+    # nlp = spacy.load("de_core_news_md", disable=["tagger", "textcat", "ner"], n_process=-1)  # only keep parser
 
     prodigy_data = []
 
     decision = df.loc[0]
 
+    used_sections = []  # sections which were already found
+    current_section = "header"
+    for paragraph in decision.paragraphs:
+        # update the current section
+        for section, markers in section_markers.items():
+            for marker in markers:
+                if marker in paragraph and section not in used_sections:
+                    current_section = section  # change to the next section
+                    used_sections.append(section)  # make sure one section is only used once
+
+        # construct the dict
+        paragraph_data = {"text": paragraph, "meta": {
+            "section": current_section,
+            "court": decision.court,
+            "title": decision.title,
+            "file_type": decision.file_type,
+            "file_name": decision.file_name,
+            "language": decision.language,
+        }}
+        prodigy_data.append(paragraph_data)
+
+    """
     sentences = list(nlp(decision.text).sents)
 
     for sent in sentences:
         source = f"{decision.court}: {decision.title}"
-        sentence_data = {"text": sent.text, "meta": {
+        sentence_data = {"text": sent.raw_text, "meta": {
             "source": source,
             "court": decision.court,
             "title": decision.title,
@@ -68,6 +144,7 @@ def bring_to_prodigy_format(df):
             "language": decision.language,
         }}
         prodigy_data.append(sentence_data)
+    """
 
     save_to_jsonl(prodigy_data, 'bger.jsonl')
 
