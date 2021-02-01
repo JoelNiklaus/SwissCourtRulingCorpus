ea68fe955c7fa5a75825d0cc745c713230075d7e
Joel Niklaus <me@joelniklaus.ch>
2021-02-01 11:15:44 +0100

    added code to scrape all the decision files from entscheidsuche

---
+++ scrc/scrape.py
@@ -0,0 +1,76 @@
+import configparser
+from pathlib import Path
+
+import bs4
+import requests
+
+from root import ROOT_DIR
+from scrc.utils.log_utils import get_logger
+from scrc.utils.main_utils import save_to_path
+
+base_url = "https://entscheidsuche.ch/"
+
+supported_suffixes = ['.htm', '.html', '.pdf', '.txt', '.json']
+supported_languages = ['de', 'fr', 'it']
+excluded_link_names = ['Name', 'Last modified', 'Size', 'Description', 'Parent Directory', 'Index', 'Jobs']
+
+logger = get_logger(__name__)
+
+
+def download_subfolders(url: str, data_dir: Path):
+    """
+    Download entire subfolders recursively
+    :param url:
+    :return:
+    """
+    logger.info(f"Downloading from {url}")
+    r = requests.get(url)  # get starting page
+    data = bs4.BeautifulSoup(r.text, "html.parser")  # parse html
+    links = data.find_all("a")  # find all links
+    logger.info(f"Found {len(links)} links")
+    for link in links:
+        if not link_is_excluded(link.text):
+            download_files(Path(link["href"]), data_dir)  # download all files in that subfolder
+
+
+def link_is_excluded(link_text: str):
+    """ Exclude links other than the folders to the courts """
+    if '.' in link_text:  # exclude filenames
+        return True
+    if len(link_text) < 3:  # exclude . and ..
+        return True
+    for excluded in excluded_link_names:
+        if excluded in link_text:  # exclude blacklisted link names
+            return True
+    return False
+
+
+def download_files(sub_folder: Path, data_dir: Path):
+    """
+    Download files from entscheidsuche
+
+    :param sub_folder:
+    :return:
+    """
+    logger.info(f"Downloading from {sub_folder} ...")
+    r = requests.get(f"{base_url}/{sub_folder}")  # get starting page
+    data = bs4.BeautifulSoup(r.text, "html.parser")  # parse html
+    links = data.find_all("a")  # find all links
+    logger.info(f"Found {len(links)} links")
+    for link in links:
+        url = Path(link["href"])  # get link url
+        if url.suffix in supported_suffixes:  # only links to files
+            try:
+                r = requests.get(base_url + str(url))  # make request to download file
+                # save to the last two parts of the url (folder and filename)
+                save_to_path(r.content, data_dir / Path(*url.parts[-2:]))
+            except Exception as e:
+                logger.error(f"Caught an exception while processing {str(url)}\n{e}")
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read(ROOT_DIR / 'config.ini')  # this stops working when the script is called from the src directory!
+    data_dir = ROOT_DIR / config['dir']['data_dir']
+
+    download_subfolders(base_url + "docs/", data_dir)
---
+++ scrc/utils/main_utils.py
@@ -0,0 +1,42 @@
+import json
+
+import pandas as pd
+
+from scrc.utils.log_utils import get_logger
+
+logger = get_logger()
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+
+def save_to_path(content, path, overwrite=False):
+    """
+    Create the parent directories of they do not exist.
+    If file does not exist already, save content to path.
+    :param content:     content to be saved
+    :param path:        path of file to be saved
+    :param overwrite:   if True overwrites the current content at that path
+    :return:
+    """
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    # check if
+    if path.exists():
+        logger.debug(f"Path {path} exists already")
+        if overwrite:
+            logger.debug("Overwrite option specified")
+        else:
+            return
+
+    logger.debug(f"Saving file to {path}")
+    # actually do the saving
+    if isinstance(content, bytes):
+        path.write_bytes(content)
+    elif isinstance(content, str):
+        path.write_text(content)
+    elif isinstance(content, dict):
+        path.write_text(json.dumps(content))
+    else:
+        raise ValueError(f"Invalid data type {type(content)} supplied.")
