516c9a26a29c37e9648d165c4a095e7ba063152a
Joel Niklaus <me@joelniklaus.ch>
2021-02-18 10:56:20 +0100

    improved parallelism in scraper.py

--- scrc/scraper.py
+++ scrc/scraper.py
@@ -19,10 +19,12 @@ logger = get_logger(__name__)
 
 
 class Scraper:
+    """Scrapes the court rulings with the associated metadata files from entscheidsuche.ch/docs"""
 
     def __init__(self, config: dict):
         self.data_dir = ROOT_DIR / config['dir']['data_dir']
         self.courts_dir = self.data_dir / config['dir']['courts_subdir']  # we save the output here
+        self.courts_dir.mkdir(parents=True, exist_ok=True)  # create output folder if it does not exist yet
 
     def download_subfolders(self, url: str):
         """
@@ -30,17 +32,17 @@ class Scraper:
         :param url:
         :return:
         """
-        logger.info(f"Downloading from {url}")
+        logger.info(f"Started downloading from {url}")
         r = requests.get(url)  # get starting page
         data = bs4.BeautifulSoup(r.text, "html.parser")  # parse html
         links = data.find_all("a")  # find all links
         included_links = [Path(link["href"]) for link in links if not self.link_is_excluded(link.text)]
         logger.info(f"Found {len(included_links)} links")
 
-        # process each court in its own process to speed up this creation by a lot!
-        pool = multiprocessing.Pool()
-        pool.map(self.download_files, included_links)
-        pool.close()
+        for link in included_links:
+            self.download_files(link)
+
+        logger.info(f"Finished downloading from {url}")
 
     def link_is_excluded(self, link_text: str):
         """ Exclude links other than the folders to the courts """
@@ -60,21 +62,28 @@ class Scraper:
         :param sub_folder:
         :return:
         """
-        logger.info(f"Downloading from {sub_folder} ...")
+        logger.info(f"Started downloading from {sub_folder} ...")
         r = requests.get(f"{base_url}/{sub_folder}")  # get starting page
         data = bs4.BeautifulSoup(r.text, "html.parser")  # parse html
         links = data.find_all("a")  # find all links
-        logger.info(f"Found {len(links)} links")
-        for link in links:
-            url = Path(link["href"])  # get link url
-            if url.suffix in supported_suffixes:  # only links to files
-                try:
-                    r = requests.get(base_url + str(url))  # make request to download file
-                    # save to the last two parts of the url (folder and filename)
-                    save_to_path(r.content, self.courts_dir / Path(*url.parts[-2:]))
-                except Exception as e:
-                    logger.error(f"Caught an exception while processing {str(url)}\n{e}")
+        included_links = [Path(link["href"]) for link in links if Path(link["href"]).suffix in supported_suffixes]
+        logger.info(f"Found {len(included_links)} links")
+
+        # process each court in its own process to speed up this creation by a lot!
+        with multiprocessing.Pool() as pool:
+            pool.map(self.download_file_from_url, included_links)
+
         logger.info(f"Finished downloading from {sub_folder} ...")
+
+    def download_file_from_url(self, url):
+        """download the file from a link and save it"""
+        logger.debug(f"Downloading from {url}")
+        try:
+            r = requests.get(base_url + str(url))  # make request to download file
+            # save to the last two parts of the url (folder and filename)
+            save_to_path(r.content, self.courts_dir / Path(*url.parts[-2:]))
+        except Exception as e:
+            logger.error(f"Caught an exception while processing {str(url)}\n{e}")
 
 
 if __name__ == '__main__':
