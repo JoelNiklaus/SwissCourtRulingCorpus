6a4c60992334c25f8fa1f61cebf45c7c8dbfd829
Joel Niklaus <me@joelniklaus.ch>
2021-04-09 22:54:06 +0200

    fixed some problems in spacy_pipeline_runner.py

--- scrc/dataset_construction/spacy_pipeline_runner.py
+++ scrc/dataset_construction/spacy_pipeline_runner.py
@@ -32,7 +32,7 @@ class SpacyPipelineRunner(DatasetConstructorComponent):
 
         self.models = {
             'de': de_core_news_lg.load(disable=disable_pipes),
-            # 'fr': fr_core_news_lg.load(disable=disable_pipes),
+            'fr': fr_core_news_lg.load(disable=disable_pipes),
             'it': it_core_news_lg.load(disable=disable_pipes)
         }
         self.active_lang_model = None
@@ -40,40 +40,36 @@ class SpacyPipelineRunner(DatasetConstructorComponent):
         self.active_collection = None
 
     def run_pipeline(self):
-        num_cpus = multiprocessing.cpu_count()
-
         db = self.get_db()
-        print("Connected to MongoDB")
+        self.logger.info("Connected to MongoDB")
         for lang in self.languages:
             self.active_lang_model = self.models[lang]  # retrieve the spacy model
             self.active_lang_model.max_length = 2000000  # increase max length for long texts
 
             self.active_collection = db[lang]
-            query = {"court": "TI_TCA"}
-            cursor = self.active_collection.find(query, {"_id": 1, "text": 1, "spacy_doc": 1}).limit(
-                5)  # fetch only texts
+
+            query = {}
+            cursor = self.active_collection.find(query, {"_id": 1, "text": 1})  # fetch only texts
             num_docs = self.active_collection.count_documents(query)
             assert num_docs > 0
             generator = self.cursor_to_generator(cursor)
 
             self.logger.info("Running spacy pipeline on the texts")
-            for doc, id in tqdm(
-                    self.active_lang_model.pipe(generator, n_process=-1, batch_size=1, as_tuples=True),
-                    total=num_docs):
+            run_spacy_pipe = self.active_lang_model.pipe(generator, n_process=-1, batch_size=1, as_tuples=True)
+            for doc, id in tqdm(run_spacy_pipe, total=num_docs):
                 self.active_collection.update_one({"_id": id}, {"$set": {"spacy_doc": doc.to_bytes()}})
 
-            filename = "it_vocab_bytes"
+            filename = f"{lang}_vocab_bytes"
             self.logger.info(f"Storing the vocab on GridFS under filename {filename}")
             fs = gridfs.GridFS(db)
             fs.put(self.active_lang_model.vocab.to_bytes(), filename=filename)
 
-            # reconstruct vocab
-            vocab_bytes = fs.find_one({"filename": filename}).read()
-            vocab = Vocab.from_bytes(vocab_bytes)
-            # reconstruct doc
-            document = self.active_collection.find_one({"court": "TI_TCA"})
-            doc = Doc(vocab).from_bytes(document['spacy_doc'])
-
+        # reconstruct vocab
+        # vocab_bytes = fs.find_one({"filename": "de_vocab_bytes"}).read()
+        # vocab = Vocab.from_bytes(vocab_bytes)
+        # reconstruct doc
+        # document = self.active_collection.find_one({"court": "TI_TCA"})
+        # doc = Doc(vocab).from_bytes(document['spacy_doc'])
 
     def cursor_to_generator(self, cursor):
         self.logger.info("Transforming cursor to generator")
