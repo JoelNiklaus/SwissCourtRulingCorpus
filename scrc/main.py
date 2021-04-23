91747ac4ee90beed85d908768c2972377cfe8f92
Joel Niklaus <me@joelniklaus.ch>
2021-04-23 15:10:48 +0200

    removed obsolete pipeline parts because of early import into db

--- scrc/main.py
+++ scrc/main.py
@@ -1,16 +1,35 @@
 import configparser
+import faulthandler
 
 from root import ROOT_DIR
 from scrc.dataset_construction.aggregator import Aggregator
 from scrc.dataset_construction.cleaner import Cleaner
+from scrc.dataset_construction.db_importer import DBImporter
 from scrc.dataset_construction.extractor import Extractor
 from scrc.dataset_construction.kaggle_dataset_creator import KaggleDatasetCreator
 from scrc.dataset_construction.scraper import Scraper, base_url
 from scrc.dataset_construction.spacy_pipeline_runner import SpacyPipelineRunner
 from scrc.dataset_construction.splitter import Splitter
 
+from filprofiler.api import profile
+
+"""
+New approach:
+- Scrape data into spider folders
+- Extract text and metadata content (save to Postgres DB because of easy interoperability with pandas (filtering and streaming))
+- Clean text  (keep raw content in db)
+- Process each text with spacy, save doc to disk and store path in db, store num token count in separate db col
+doc.to_disk("/path/to/doc", exclude=['tensor']) # think about excluding tensor to save space (almost 4x less space)
+- split BGer into sections (from html_raw)
+- extract citations (from raw text)
+
+
+"""
+
 
 def main():
+    # faulthandler.enable()  # can print a minimal threaddump in case of external termination
+
     config = configparser.ConfigParser()
     config.read(ROOT_DIR / 'config.ini')  # this stops working when the script is called from the src directory!
 
@@ -23,17 +42,11 @@ def main():
     cleaner = Cleaner(config)
     cleaner.clean()
 
-    splitter = Splitter(config)
-    splitter.split_spiders()
-
-    aggregator = Aggregator(config)
-    aggregator.combine_spiders()
-
     spacy_pipeline_runner = SpacyPipelineRunner(config)
     spacy_pipeline_runner.run_pipeline()
 
-    kaggle_dataset_creator = KaggleDatasetCreator(config)
-    kaggle_dataset_creator.create_dataset()
+    # kaggle_dataset_creator = KaggleDatasetCreator(config)
+    # kaggle_dataset_creator.create_dataset()
 
 
 if __name__ == '__main__':
