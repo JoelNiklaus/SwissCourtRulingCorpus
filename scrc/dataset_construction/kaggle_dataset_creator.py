3ce06eac00627d48cfff4dae4075f214df8c19ff
Joel Niklaus <me@joelniklaus.ch>
2021-05-11 16:43:10 +0200

    added code for judgement prediction and for citation prediction datasets

--- scrc/dataset_construction/kaggle_dataset_creator.py
+++ scrc/dataset_construction/kaggle_dataset_creator.py
@@ -14,6 +14,7 @@ import numpy as np
 from root import ROOT_DIR
 from scrc.dataset_construction.dataset_constructor_component import DatasetConstructorComponent
 from scrc.utils.log_utils import get_logger
+import json
 
 """
 Datasets to be created:
@@ -27,7 +28,7 @@ Datasets to be created:
         - Zero/One/Few Shot
 - Chamber/court prediction => proxy for legal area
     - text classification
-    - input (entire text) to label (chamber/court code)
+    - input (situation) to label (chamber/court code)
     - Features:
         - Zero/One/Few Shot
 - Date prediction
@@ -36,7 +37,7 @@ Datasets to be created:
     - Features:
         - Zero/One/Few Shot
 - Section splitting: 
-    - token classification
+    - token classification (text zoning task)
     - input (entire text) to label (section tags per token)
 - LM Pretraining:
     - masked language modeling
@@ -67,19 +68,63 @@ class KaggleDatasetCreator(DatasetConstructorComponent):
         self.seed = 42
 
     def create_datasets(self):
-        self.creating_judgement_prediction_dataset()
-
-    def creating_judgement_prediction_dataset(self):
-        self.logger.info("Creating Judgement Prediction Dataset")
-        self.logger.info("Reading df from database")
         engine = self.get_engine(self.db_scrc)
+
+        datasets = ['judgement_prediction', 'citation_prediction', ]
+        # 'chamber_prediction', 'date_prediction','section_splitting']
+        processed_file_path = self.data_dir / f"datasets_created.txt"
+        datasets, message = self.compute_remaining_parts(processed_file_path, datasets)
+        self.logger.info(message)
+
+        for dataset in datasets:
+            self.create_dataset(engine, dataset)
+
+    def create_dataset(self, engine, dataset):
+        self.logger.info(f"Creating {dataset} Dataset")
+
+        create_df = getattr(self, dataset)  # get the function called by the dataset name
+
+        folder = self.create_dir(self.kaggle_subdir, dataset)
+        self.create_kaggle_dataset(create_df(engine), folder)
+
+    def judgement_prediction(self, engine):
         columns = "extract(year from date) as year, considerations, judgement"
         where = "court = 'CH_BGer' AND considerations IS NOT NULL AND judgement IS NOT NULL"
         order_by = "year"
         df = next(self.select(engine, 'de', columns=columns, where=where, order_by=order_by, chunksize=int(1e7)))
+
         df = df.rename(columns={"considerations": "text", "judgement": "label"})  # normalize column names
-        folder = self.create_dir(self.kaggle_subdir, "judgement_prediction")
-        self.create_kaggle_dataset(df, folder)
+        return df
+
+    def citation_prediction(self, engine):
+        columns = "extract(year from date) as year, text, citations"
+        where = "court = 'CH_BGer' AND text IS NOT NULL AND citations IS NOT NULL"
+        order_by = "year"
+        df = next(self.select(engine, 'de', columns=columns, where=where, order_by=order_by, chunksize=int(1e7)))
+
+        def replace_citations(series):
+            laws = series.citations['laws']
+            rulings = series.citations['rulings']
+
+            for law in laws:
+                series.text = series.text.replace(law['text'], "<law-citation>")
+            for ruling in rulings:
+                series.text = series.text.replace(ruling['text'], "<ruling-citation>")
+
+        df = df.apply(replace_citations, axis='columns')
+
+        df = df.rename(columns={"text": "text", "citations": "label"})  # normalize column names
+
+        return df
+
+    def chamber_prediction(self, engine):
+        pass
+
+    def date_prediction(self, engine):
+        pass
+
+    def section_splitting(self, engine):
+        pass
 
     def create_kaggle_dataset(self, df, folder, split_type="date-stratified"):
         """
@@ -109,14 +154,20 @@ class KaggleDatasetCreator(DatasetConstructorComponent):
         sample_submission = solution.rename(columns={"Expected": "Predicted"})  # rename according to kaggle conventions
         sample_submission['Predicted'] = np.random.choice(list(labels), size=len(solution))  # set to random value
 
-        # save to csv
+        # save to folder
         train.to_csv(folder / 'train.csv', index_label='Id')
         val.to_csv(folder / 'val.csv', index_label='Id')
         test.to_csv(folder / 'test.csv', index_label='Id')
         solution.to_csv(folder / 'solution.csv', index_label='Id')
         sample_submission.to_csv(folder / 'sampleSubmission.csv', index_label='Id')
-        labels.to_csv(folder / 'labels.csv', index_label='Id')
+        self.save_labels(labels, folder / 'labels.json')
         self.logger.info(f"Saved files necessary for competition to {folder}")
+
+    def save_labels(self, labels, file_name):
+        labels_dict = dict(enumerate(labels))
+        json_labels = {"idx2label": labels_dict, "label2idx": {y: x for x, y in labels_dict.items()}}
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(json_labels, f, ensure_ascii=False, indent=4)
 
     def split_date_stratified(self, df, split=(0.7, 0.1, 0.2)):
         last_year = 2020  # disregard partial year 2021
