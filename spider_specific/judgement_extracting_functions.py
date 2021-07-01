b5dae4e3804978260befc5d652d8ec2b150de9f7
Joel Niklaus <me@joelniklaus.ch>
2021-07-01 13:59:59 +0200

    added Adrian's code and added some marker strings and some comments

--- spider_specific/judgement_extracting_functions.py
+++ spider_specific/judgement_extracting_functions.py
@@ -1,8 +1,7 @@
-from collections import OrderedDict
 from typing import Optional, List
 
 import re
-from scrc.utils.main_utils import string_contains_one_of_list, clean_text
+from scrc.utils.main_utils import string_contains_one_of_list, clean_text, int_to_roman
 
 """
 
@@ -33,7 +32,8 @@ Hauptbegehren:
     - Teilweise Abweisung (partial_dismissal) (materiell/inhaltlich): Ein Teil des Begehrens wird abgewiesen
     - Nichteintreten (inadmissible) (formell): Das Gericht ist nicht zuständig für das Begehren, Formelle Mängel der Beschwerde 
     - Abschreibung (write_off) (formell): Gegenstandslosigkeit, Kein Grund für das Verfahren (der Entscheid wird nicht mehr benötigt, da bspw. die Parteien sich aussergerichtlich geeinigt haben oder da zwei Verfahren vereinigt werden)
-
+    - Vereinigung (unification) (formell): When two cases are about the same thing, they will get merged into one.
+    
 Nebenbegehren:
     - Gerichtskosten
         - Nicht Erhoben 
@@ -51,20 +51,52 @@ Formelle Mitteilung:
 """
 
 judgement_markers = {
-    'de': {'approval': ['aufgehoben', 'aufzuheben', 'gutgeheissen', 'gutzuheissen', 'In Gutheissung'],
-           'partial_approval': ['teilweise gutgeheissen', 'teilweise gutzuheissen',
-                                'In teilweiser Gutheissung'],
-           'dismissal': ['abgewiesen', 'abzuweisen'],
-           'partial_dismissal': ['abgewiesen, soweit darauf einzutreten ist',
-                                 'abzuweisen, soweit darauf einzutreten ist',
-                                 'abgewiesen, soweit auf sie einzutreten ist',
-                                 'abzuweisen, soweit auf sie einzutreten ist'],
-           'inadmissible': ['Nichteintreten', 'nicht eingetreten', 'nicht einzutreten',
-                            'wird nicht eingegangen', 'wird keine Folge geleistet',
-                            'soweit darauf einzutreten ist', 'soweit auf sie einzutreten ist'],
-           'write_off': ['abgeschrieben', 'abzuschreiben', 'erweist sich als gegenstandslos']},
-    'fr': {},
-    'it': {}
+    'de': {
+        'approval': ['aufgehoben', 'aufzuheben', 'gutgeheissen', 'gutzuheissen', 'In Gutheissung'],
+        'partial_approval': ['teilweise gutgeheissen', 'teilweise gutzuheissen',
+                             'In teilweiser Gutheissung'],
+        'dismissal': ['abgewiesen', 'abzuweisen'],
+        'partial_dismissal': ['abgewiesen, soweit darauf einzutreten ist',
+                              'abzuweisen, soweit darauf einzutreten ist',
+                              'abgewiesen, soweit auf sie einzutreten ist',
+                              'abzuweisen, soweit auf sie einzutreten ist'],
+        'inadmissible': ['Nichteintreten', 'nicht eingetreten', 'nicht einzutreten',
+                         'wird keine Folge geleistet', 'wird nicht eingegangen',
+                         'soweit darauf einzutreten ist', 'soweit auf sie einzutreten ist'],
+        'write_off': ['abgeschrieben', 'abzuschreiben', 'erweist sich als gegenstandslos'],
+        'unification': ["werden vereinigt", "werden gemeinsam beurteilt", "werden nicht vereinigt"]
+    },
+    'fr': {
+        'approval': ['admis', 'est annulé', 'Admet'],
+        'partial_approval': ['Admet partiellement',
+                             'partiellement admis',
+                             'admis dans la mesure où il est recevable',
+                             'admis dans la mesure où ils sont recevables'
+                             ],
+        'dismissal': ['rejeté', 'Rejette', 'écarté'],
+        'partial_dismissal': ['rejetés dans la mesure où ils sont recevables',
+                              'rejeté, dans la mesure où il est recevable',
+                              'rejeté dans la mesure où il est recevable',
+                              'rejeté dans la mesure de sa recevabilité'
+                              ],
+        'inadmissible': ['N\'entre pas en matière', 'irrecevable', 'n\'est pas entré', 'pas pris en considération'],
+        'write_off': ['retrait', 'est radiée', 'sans objet', 'rayé', 'Raye'],
+        'unification': [],
+    },
+    'it': {
+        'approval': ['accolto', 'accolti', 'accolta', 'è annullato'],
+        'partial_approval': ['Nella misura in cui è ammissibile, il ricorso è parzialmente accolto',
+                             'In parziale accoglimento del ricorso'],
+        'dismissal': ['respinto', 'respinti', 'respinta'],
+        'partial_dismissal': ['Nella misura in cui è ammissibile, il ricorso è respinto',
+                              'Nella misura in cui è ammissibile, il ricorso di diritto pubblico è respinto',
+                              'Nella misura in cui è ammissibile, la domanda di revisione è respinta'],
+        'inadmissible': ['inammissibile', 'inammissibili', 'inammissibila'],
+        'write_off': ['Il ricorso è divenuto privo d\'oggetto', 'Il ricorso è diventato privo d\'oggetto',
+                      'in quanto divenuta priva d\'oggetto',
+                      'La causa è stralciata dai ruoli a seguito del ritiro del ricorso'],
+        'unification': ['sono congiunte'],
+    }
 }
 
 """
@@ -73,62 +105,90 @@ The name of the functions should be equal to the spider! Otherwise, they won't b
 """
 
 
+def get_judgements(rulings: str, namespace: dict) -> set:
+    """
+    Get the judgment outcomes based on a rulings string and the given namespace context.
+    :param rulings:     the rulings string
+    :param namespace:   the context (metadata) from the court decision
+    :return:            the set of judgment outcomes
+    """
+    judgements = set()
+
+    n = 1
+    while len(judgements) == 0:
+        try:
+            # Only look at main ruling (the first one) because it is by far the most important one for the case
+            main_ruling = get_nth_ruling(rulings, namespace, n)
+
+            for judgement, markers in judgement_markers[namespace['language']].items():
+                # TODO maybe we should change to regex search here to make it easier with declinations in fr and it
+                if string_contains_one_of_list(main_ruling, markers):
+                    judgements.add(judgement)
+            n = n + 1
+        except ValueError:
+            break
+    return judgements
+
+
+def get_nth_ruling(rulings: str, namespace: dict, n: int) -> str:
+    """
+    Gets the nth ruling from the rulings string
+    :param rulings:     the rulings string
+    :param namespace:   the context (metadata) from the court decision
+    :param n:           the nth ruling to be retrieved
+    :return:            the string of the nth ruling
+    """
+    result = search_rulings(rulings, str(n), str(n + 1))
+    if not result:
+        # try with roman numerals
+        result = search_rulings(rulings, int_to_roman(n), int_to_roman(n + 1))
+        if not result:
+            message = f"For the decision {namespace['html_url']} no main ruling was found from the rulings: {rulings}"
+            raise ValueError(message)
+    return result.group(1)
+
+
+def search_rulings(rulings: str, start: str, end: str):
+    """
+    Search the rulings for start and end indicating the boundaries of the ruling to be found
+    :param rulings: the rulings string
+    :param start:   the string indicating the start of the ruling to be found
+    :param end:     the string indicating the end of the ruling to be found
+    :return:        the ruling between the start and end string
+    """
+    pattern = rf"{start}\.(.+?)(?:{end}\.|$)"
+    return re.search(pattern, rulings)
+
+
 def CH_BGer(rulings: str, namespace: dict) -> Optional[List[str]]:
     """
-    IMPORTANT: So far, only German is supported!
+    Extract judgement outcomes from the Federal Supreme Court of Switzerland
     :param rulings:     the string containing the rulings
     :param namespace:   the namespace containing some metadata of the court decision
     :return:            the sections dict, None if not in German
     """
-    # TODO extend to fr and it
-    if namespace['language'] != 'de':
-        return None
 
-    rulings = clean_text(rulings)  # make sure we don't have any nasty unicode problems
+    if namespace['language'] not in judgement_markers:
+        message = f"This function is only implemented for the languages {list(judgement_markers.keys())} so far."
+        raise ValueError(message)
 
-    # Only look at main ruling (the first one) because it is by far the most important one for the case
-    main_ruling = get_nth_ruling(rulings, namespace, 1)
+    # make sure we don't have any nasty unicode problems
+    rulings = clean_text(rulings)
 
-    # When two cases are about the same thing, they will get merged into one. The main ruling is the second one in that case
-    combination = ["werden vereinigt", "werden gemeinsam beurteilt", "werden nicht vereinigt"]
-    if string_contains_one_of_list(main_ruling, combination):
-        main_ruling = get_nth_ruling(rulings, namespace, 2)
-
-    judgements = set()
-    for judgement, markers in judgement_markers[namespace['language']].items():
-        if string_contains_one_of_list(main_ruling, markers):
-            judgements.add(judgement)
+    judgements = get_judgements(rulings, namespace)
 
     if not judgements:
-        message = f"Found no judgement for the rulings {rulings} in the case {namespace['html_url']}. Please check!"
+        message = f"Found no judgement for the rulings \"{rulings}\" in the case {namespace['html_url']}. Please check!"
         raise ValueError(message)
     elif len(judgements) > 1:
         if "partial_approval" in judgements:
-            judgements.discard("approval")  # if partial_approval is found, it will find approval as well
+            # if partial_approval is found, it will find approval as well
+            judgements.discard("approval")
         if "partial_dismissal" in judgements:
-            judgements.discard("dismissal")  # if partial_dismissal is found, it will find dismissal as well
-
-    # We allow multiple judgements in all combinations
-    # if len(judgements) > 1:
-    #    message = f"Found multiple judgements {judgements} for the case {namespace['html_url']}. Please check!"
-    #    raise ValueError(message)
+            # if partial_dismissal is found, it will find dismissal as well
+            judgements.discard("dismissal")
 
     return list(judgements)
-
-
-def get_nth_ruling(rulings, namespace, n):
-    result = search_rulings(rulings, str(n), str(n + 1))
-    if not result:
-        result = search_rulings(rulings, int_to_roman(n), int_to_roman(n + 1))  # try with roman numerals
-        if not result:
-            raise ValueError(
-                f"For the decision {namespace['html_url']} no main ruling was found from the rulings: {rulings}")
-    return result.group(1)
-
-
-def search_rulings(rulings, start_str, end_str):
-    pattern = rf"{start_str}\.(.+?)(?:{end_str}\.|$)"
-    return re.search(pattern, rulings)
 
 # This needs special care
 # def CH_BGE(rulings: str, namespace: dict) -> Optional[List[str]]:
