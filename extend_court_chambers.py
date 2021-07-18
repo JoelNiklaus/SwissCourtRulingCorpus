e1aabf5b90852950e5745f780ecaea75c8d5be4e
Joel Niklaus <me@joelniklaus.ch>
2021-07-18 13:06:21 +0200

    removed Gerichtsinstanz from court_chambers_extended.json

--- extend_court_chambers.py
+++ extend_court_chambers.py
@@ -9,7 +9,7 @@ for canton_key, canton_dict in cantons.items():
         for chamber_key, chamber_dict in court_dict['kammern'].items():
             cantons[canton_key]['gerichte'][court_key]['kammern'][chamber_key]['Rechtsgebiete'] = ['']
         cantons[canton_key]['gerichte'][court_key] = OrderedDict(cantons[canton_key]['gerichte'][court_key])
-        cantons[canton_key]['gerichte'][court_key]['Gerichtsinstanz'] = ''
+        #cantons[canton_key]['gerichte'][court_key]['Gerichtsinstanz'] = ''
         cantons[canton_key]['gerichte'][court_key].move_to_end('kammern', last=True)
 
 Path("court_chambers_extended.json").write_text(json.dumps(cantons))
