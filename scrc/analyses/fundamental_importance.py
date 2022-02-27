ba7b37d2810ac4c1e75e9e506fdf8c74f65c5ba7
Joel Niklaus <me@joelniklaus.ch>
2022-02-27 23:30:25 +0100

    extracted negations to json file and improved fundamental_importance analysis

--- scrc/analyses/fundamental_importance.py
+++ scrc/analyses/fundamental_importance.py
@@ -1,5 +1,6 @@
 from typing import List, Union
 
+import json
 import nltk
 from nltk import sent_tokenize, word_tokenize
 
@@ -23,22 +24,24 @@ Two large difficulties with this analysis:
 
 class FundamentalImportanceAnalysis(AbstractPreprocessor):
 
-    def __init__(self, config: dict):
+    def __init__(self, config: dict, negation_detection_type="xlmr"):
         super().__init__(config)
         self.logger = get_logger(__name__)
 
+        self.negation_detection_type = negation_detection_type
         # Two methods: either search with these strings
         self.fundamental_importance_search_strings = {
-            "de": "Rechtsfrage von grundsätzlicher Bedeutung",
-            "fr": "question juridique de principe",
-            "it": "questione di diritto di importanza fondamentale",
+            "de": ["Rechtsfrage von grundsätzlicher Bedeutung", "Frage von grundsätzlicher Bedeutung"],
+            "fr": ["question juridique de principe", ],
+            "it": ["questione di diritto di importanza fondamentale", "questione giuridica d''importanza fondamentale"],
         }
         # or search with law articles
         self.articles = {
             # We removed Art. 42 because it is being cited many times without relevance to fundamental importance
             # Zitate sehr abhängig vom Gerichtsschreiber
             "de": [
-                "Art. 20 Abs. 2 BGG",  # "Art. 42 Abs. 2 BGG",
+                "Art. 20 Abs. 2 BGG",
+                # "Art. 42 Abs. 2 BGG", # causes too many false positives
                 "Art. 74 Abs. 2 lit. a BGG",
                 "Art. 83 Abs. 1 lit. f Ziff. 1 BGG", "Art. 83 lit. f Ziff. 1 BGG",
                 "Art. 83 Abs. 1 lit. m BGG", "Art. 83 lit. m BGG",
@@ -47,7 +50,8 @@ class FundamentalImportanceAnalysis(AbstractPreprocessor):
                 "Art. 84a BGG", "Art. 85 Abs. 2 BGG", "Art. 109 Abs. 1 BGG"
             ],
             "fr": [
-                "art. 20 al. 2 LTF",  # "art. 42 al. 2 LTF",
+                "art. 20 al. 2 LTF",
+                # "art. 42 al. 2 LTF", # causes too many false positives
                 "art. 74 al. 2 let. a LTF",
                 "art. 83 al. 1 let. f n. 1 LTF", "art. 83 let. f n. 1 LTF",
                 "art. 83 al. 1 let. m LTF", "art. 83 let. m LTF",
@@ -56,7 +60,8 @@ class FundamentalImportanceAnalysis(AbstractPreprocessor):
                 "art. 84a LTF", "art. 85 al. 2 LTF", "art. 109 al. 1 LTF"
             ],
             "it": [
-                "art. 20 cpv. 2 LTF",  # "art. 42 cpv. 2 LTF",
+                "art. 20 cpv. 2 LTF",
+                # "art. 42 cpv. 2 LTF", # causes too many false positives
                 "art. 74 cpv. 2 lett. a LTF",
                 "art. 83 cpv. 1 lett. f n. 1 LTF", "art. 83 lett. f n. 1 LTF",
                 "art. 83 cpv. 1 lett. m LTF", "art. 83 lett. m LTF",
@@ -79,14 +84,16 @@ class FundamentalImportanceAnalysis(AbstractPreprocessor):
         df = pd.DataFrame()
         for lang in ["de", "fr", "it"]:
             # strpos is faster than LIKE, which is faster than ~ (regex search)
+            where = f"spider = 'CH_BGer' AND "
             if type == "search_strings":
-                where = f"spider = 'CH_BGer' AND strpos(text, '{self.fundamental_importance_search_strings[lang]}')>0"
+                search_strings = self.fundamental_importance_search_strings[lang]
             elif type == "articles":
-                regex = "|".join(self.articles[lang])
-                where = f"spider = 'CH_BGer' AND considerations ~ '{regex}'"
+                search_strings = self.articles[lang]
             else:
                 raise ValueError("type should be either search_strings or articles")
-            columns = "language, chamber, date, text, html_url"
+            searches = [f"strpos(text, '{s}')>0" for s in search_strings]
+            where += f"({' OR '.join(searches)})"
+            columns = "language, chamber, date, html_url, paragraphs, text"
             df = df.append(next(self.select(engine, lang, columns=columns, where=where, chunksize=20000)))
 
         self.logger.info(f"Saving data to cache at {cache_file}")
@@ -107,24 +114,31 @@ class FundamentalImportanceAnalysis(AbstractPreprocessor):
                          f"and {num_entries.it} Italian decisions"
                          f" ({num_entries.sum()} in Total)")
 
-        self.logger.info("Splitting the text into sentences")
-        df = df.apply(self.sentencize, axis="columns")
+        if self.negation_detection_type == "simple":
+            self.logger.info("Splitting the text into sentences")
+            df = df.apply(self.sentencize, axis="columns")
 
-        self.logger.info("Removing the sentences that do not contain any legal question of fundamental importance")
-        df = df.apply(self.filter_by_fundamental_importance, axis="columns")
+            self.logger.info("Removing the sentences that do not contain any legal question of fundamental importance")
+            df = df.apply(self.filter_by_fundamental_importance, axis="columns")
+            fundamental_importance_parts = "fundamental_importance_sentences"
+        elif self.negation_detection_type == "xlmr":
+            self.logger.info("Removing the paragraphs that do not contain any legal question of fundamental importance")
+            df = df.apply(self.filter_by_fundamental_importance, axis="columns")
+            fundamental_importance_parts = "fundamental_importance_paragraphs"
 
         self.logger.info("Removing the decisions where no direct mention of fundamental importance is found")
-        df = df[df.fundamental_importance_sentences.apply(lambda x: len(x)) > 0]
+        df = df[df[fundamental_importance_parts].apply(lambda x: len(x)) > 0]
         self.logger.info(f"Removed {num_entries.sum() - df.text.count()} entries "
                          f"(Originally: {num_entries.sum()}, Now: {df.text.count()})")
 
-        self.logger.info("Filtering decisions containing negations in the same sentence "
+        self.logger.info("Filtering decisions containing negations in the same sentence/paragraph "
                          "as the legal question of fundamental importance was found")
         df = df.apply(self.contains_negation_fundamental_importance, axis="columns")
         df['year'] = pd.to_datetime(df.date).dt.year
         df['legal_area'] = df.chamber.apply(get_legal_area)
 
-        self.logger.info("Counting the number of negated sentences")
+        # TODO do smart aggregation (only negated if more than half is negated for example)
+        self.logger.info("Counting the number of negated occurrences of fundamental importance")
         negated_mask = pd.array([bool(entry) for entry in df.negated.tolist()], dtype="boolean")
         negated_df = df[negated_mask]
         not_negated_df = df[~negated_mask]
@@ -173,20 +187,31 @@ class FundamentalImportanceAnalysis(AbstractPreprocessor):
 
     def contains_negation_fundamental_importance(self, df):
         # TODO bessere negation detection einbauen: https://spacy.io/universe/project/negspacy, https://drive.google.com/drive/folders/1md-_WBrg9x2Kp4g6jNExLJrEt5HBGL23
-        negation_fundamental_importance_search_strings = {
-            "de": ["keine", "kein", "nicht", "mangels", "nur", "weder"],
-            "fr": ["aucune", "absence", "ne", "n'", "pas", "que", "qu'"],
-            "it": ["non", "né", ]
-        }
-        df['fundamental_importance_tokens'] = self.nltk_word_tokenize(df.fundamental_importance_sentences, df.language)
-        df['negated'] = string_contains_one_of_list(df.fundamental_importance_tokens,
-                                                    negation_fundamental_importance_search_strings[
-                                                        df.language])
+        all_negations = json.load((self.analysis_dir / 'negations.json').read_text())
+        lang = df.language
+        negations = all_negations[lang]["words"]
+        if self.negation_detection_type == 'simple':
+            df['fundamental_importance_tokens'] = self.nltk_word_tokenize(df.fundamental_importance_sentences, lang)
+            df['negated'] = string_contains_one_of_list(df.fundamental_importance_tokens, negations)
+        elif self.negation_detection_type == 'xlmr':
+
+            # TODO paragraphen als Input nehmen und nicht ganzer Text
+            # TODO negation cue listen beschaffen
+            # TODO negation scope resolution mit modellen von anastassia machen
+            # TODO aggregation pro vorkommnis des suchbegriffs machen (majority vote?)
+            pass
+        elif self.negation_detection_type == 'negex':
+            raise ValueError("Negex is not implemented yet.")
+        else:
+            raise ValueError(f"Negation detection type {self.negation_detection_type} not supported")
         return df
 
     def filter_by_fundamental_importance(self, df):
+        search_strings = self.fundamental_importance_search_strings[df.language]
         df['fundamental_importance_sentences'] = [sentence for sentence in df.sentences if
-                                                  self.fundamental_importance_search_strings[df.language] in sentence]
+                                                  search_strings in sentence]
+        df['fundamental_importance_paragraphs'] = [paragraph['text'] for paragraph in df.paragraphs if
+                                                   search_strings in paragraph['text']]
         return df
 
     def sentencize(self, df):
