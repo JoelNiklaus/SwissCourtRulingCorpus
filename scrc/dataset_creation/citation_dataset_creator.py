0d694853b327be5d6216f35ed79f7ae41d54e4f5
Joel Niklaus <me@joelniklaus.ch>
2022-02-28 10:05:14 +0100

    did some refactoring

--- scrc/dataset_creation/citation_dataset_creator.py
+++ scrc/dataset_creation/citation_dataset_creator.py
@@ -18,8 +18,18 @@ from scrc.utils.term_definitions_converter import TermDefinitionsConverter
 
 class CitationDatasetCreator(DatasetCreator):
     """
-    Creates a dataset with the text as input and the citations as labels
+    Creates a dataset for information retrieval with the citations serving as ground truth for relevance scores
     """
+
+    # usefulness of law articles still unclear.
+    # Approach of Lawyers:
+    # google
+    # get law articles
+    # get relevant case law
+    #
+    # what they want is:
+    # - from facts of a case get other cases
+    # - from law article get cases that cite this article ==> starting with newest case (lawyer can follow back)
 
     def __init__(self, config: dict):
         super().__init__(config)
@@ -32,6 +42,10 @@ class CitationDatasetCreator(DatasetCreator):
 
         self.num_ruling_citations = 1000  # the 1000 most common ruling citations will be included
 
+        self.load_rulings()
+        self.load_law_articles()
+
+    def load_rulings(self):
         self.logger.info(f"Loading reference rulings")
         self.available_bges = set()  # use set instead of list for faster lookup
         rulings = pd.DataFrame()
@@ -45,47 +59,49 @@ class CitationDatasetCreator(DatasetCreator):
             self.available_bges.update(df.file_number.tolist())
             rulings = pd.concat([rulings, df], ignore_index=True)  # one rulings df for all languages
 
-        # TODO change paths where to save the dataset
-        rulings_path = ROOT_DIR / "scrc/dataset_creation/rulings.jsonl"
+        rulings_path = self.create_dir(self.datasets_subdir, self.dataset_name) / "rulings.jsonl"
         if not rulings_path.exists():
             rulings.to_json(rulings_path, orient="records", lines=True, date_format="iso")
 
+    def load_law_articles(self):
         # get Art. from lexfind.jsonl
         self.logger.info(f"Loading reference law articles")
-        laws = pd.read_json(ROOT_DIR / "scrc/dataset_creation/lexfind.jsonl", lines=True)
+        laws = pd.read_json(self.corpora_subdir / "lexfind.jsonl", lines=True)
         laws = laws[laws.canton.str.contains("ch")]  # only federal laws so far
         laws = laws[laws.abbreviation.str.len() > 1]  # only keep the ones with an abbreviation
         self.available_laws = set(laws.abbreviation.unique().tolist())
 
-        def extract_article_info(law):
-            arts = {"canton": [], "language": [], "sr_number": [], "abbreviation": [], "short": [], "title": [],
-                    "link": [], "text": [], }
-            bs = BeautifulSoup(law.html_content, "html.parser")
-            for art in bs.find_all("article"):
-                arts["canton"].append(law.canton)
-                arts["language"].append(law.language)
-                arts["sr_number"].append(law.sr_number)
-                arts["abbreviation"].append(law.abbreviation)
-                arts["short"].append(law.short)
-                arts["title"].append(law.title)
-
-                link = art.find(fragment=f"#{art['id']}")
-                if link:
-                    arts["link"].append(link['href'])
-                else:
-                    arts["link"].append(None)
-                text = art.find("div", {"class": "collapseable"})
-                if text:
-                    arts["text"].append(text.get_text())
-                else:
-                    arts["text"].append(None)
-            return pd.DataFrame(arts)
-
-        articles_path = ROOT_DIR / "scrc/dataset_creation/articles.jsonl"
+        articles_path = self.create_dir(self.datasets_subdir, self.dataset_name) / "articles.jsonl"
         if not articles_path.exists():
             self.logger.info("Extracting individual articles from laws")
-            articles = pd.concat([extract_article_info(row) for _, row in laws.iterrows()], ignore_index=True)
+            articles = pd.concat([self.extract_article_info(row) for _, row in laws.iterrows()], ignore_index=True)
             articles.to_json(articles_path, orient="records", lines=True)
+
+    @staticmethod
+    def extract_article_info(law):
+        """Extracts the information for the individual articles from a given law"""
+        arts = {"canton": [], "language": [], "sr_number": [], "abbreviation": [], "short": [], "title": [],
+                "link": [], "text": [], }
+        bs = BeautifulSoup(law.html_content, "html.parser")
+        for art in bs.find_all("article"):
+            arts["canton"].append(law.canton)
+            arts["language"].append(law.language)
+            arts["sr_number"].append(law.sr_number)
+            arts["abbreviation"].append(law.abbreviation)
+            arts["short"].append(law.short)
+            arts["title"].append(law.title)
+
+            link = art.find(fragment=f"#{art['id']}")
+            if link:
+                arts["link"].append(link['href'])
+            else:
+                arts["link"].append(None)
+            text = art.find("div", {"class": "collapseable"})
+            if text:
+                arts["text"].append(text.get_text())
+            else:
+                arts["text"].append(None)
+        return pd.DataFrame(arts)
 
     def get_dataset(self, feature_col, lang, save_reports):
         df = self.get_df(self.get_engine(self.db_scrc), feature_col, 'citations', lang, save_reports)
@@ -94,74 +110,11 @@ class CitationDatasetCreator(DatasetCreator):
         # df = df[df.types.map(len) >= 1]  # we only want the decisions which actually cite something
         # df = df.query('"bge" in types')  # ensure that we only have BGE citations
 
-        def get_ruling_citations(citations):
-            # get BGEs by file_number
-            cits = []
-            for citation in citations['rulings']:
-                cit = citation['text']
-                cit = ' '.join(cit.split())  # remove multiple whitespaces inside
-                try:
-                    ruling_cit = RulingCitation(cit)
-                except ValueError as ve:
-                    self.logger.warning(ve)
-                    continue
-                # only actually include citations that we can find in our corpus
-                if str(ruling_cit) in self.available_bges:
-                    cits.append(ruling_cit)
-            if cits:  # only return something if we actually have citations
-                return list(Counter(cits).items())
-
-        def get_law_citations(citations):
-            cits = []
-            for citation in citations['laws']:
-                cit = citation['text']
-                cit = ' '.join(cit.split())  # remove multiple whitespaces inside
-                try:
-                    law_cit = LawCitation(cit)
-                except ValueError as ve:
-                    self.logger.warning(ve)
-                    continue
-                # only actually include citations that we can find in our corpus
-                if law_cit.abbreviation in self.available_laws:
-                    cits.append(law_cit)
-            if cits:  # only return something if we actually have citations
-                return list(Counter(cits).items())
-
-        df['rulings'] = df.citations.apply(get_ruling_citations)
-        df['laws'] = df.citations.apply(get_law_citations)
+        df['rulings'] = df.citations.apply(self.get_ruling_citations)
+        df['laws'] = df.citations.apply(self.get_law_citations)
         df = df.dropna(subset=["rulings", "laws"], how="all")  # we cannot use the ones which have no citations
 
-        def compute_relevance_score(citation_tuple):
-            """
-            Computes a relevance score for the citation between 1 and 1
-                frequency: 70%
-                year: 30%
-            :return:
-            # Proxy für relevanz score:
-            #   - häufigkeit des Zitats in Erwägungen
-            #   - neuere Urteile sind relevanter (ältere Urteile sind evtl. überholt oder nicht mehr relevant)
-            #   Thomas schreibt mir seine Ideen bald
-            #   - BGE wichtiger als andere ==> nur BGEs nehmen
-            """
-            weights = {"frequency": 0.7, "year": 0.3}
-            assert sum(weights.values()) == 1
-
-            values = {"frequency": citation_tuple[1] / 10}
-            citation = citation_tuple[0]
-            if isinstance(citation, RulingCitation):
-                # 150 corresponds to the year 2024 which should give use enough leeway
-                values["year"] = citation.year / 150
-            else:
-                values["year"] = 0.5  # set it to middle importance for all law citations
-
-            # compute weighted sum
-            score = 0
-            for key in weights.keys():
-                score += weights[key] * values[key]
-
-            return round(min(score, 1), 2)  # cap it at 1 and round to 2 decimals
-
-        relevance_lambda = lambda cits: [(cit_tuple[0], compute_relevance_score(cit_tuple))
+        relevance_lambda = lambda cits: [(cit_tuple[0], self.compute_relevance_score(cit_tuple))
                                          for cit_tuple in cits] if cits else None
         df.rulings = df.rulings.apply(relevance_lambda)
         df.laws = df.laws.apply(relevance_lambda)
@@ -169,16 +122,6 @@ class CitationDatasetCreator(DatasetCreator):
         df = df.drop(['citations'], axis=1)  # we don't need this anymore
 
         # TODO extract ruling citations from other decisions and test the extraction regexes on the CH_BGer
-
-        # usefulness of law articles still unclear.
-        # Approach of Lawyers:
-        # google
-        # get law articles
-        # get relevant case law
-        #
-        # what they want is:
-        # - from facts of a case get other cases
-        # - from law article get cases that cite this article ==> starting with newest case (lawyer can follow back)
 
         # this_function_name = inspect.currentframe().f_code.co_name
         folder = self.create_dir(self.datasets_subdir, self.dataset_name)
@@ -216,6 +159,70 @@ class CitationDatasetCreator(DatasetCreator):
         df = df.rename(columns={feature_col: "text"})  # normalize column names
         df['label'] = df.rulings  # the label is just the rulings for now
         return df, self.available_bges
+
+    def get_ruling_citations(self, citations):
+        # get BGEs by file_number
+        cits = []
+        for citation in citations['rulings']:
+            cit = citation['text']
+            cit = ' '.join(cit.split())  # remove multiple whitespaces inside
+            try:
+                ruling_cit = RulingCitation(cit)
+            except ValueError as ve:
+                self.logger.warning(ve)
+                continue
+            # only actually include citations that we can find in our corpus
+            if str(ruling_cit) in self.available_bges:
+                cits.append(ruling_cit)
+        if cits:  # only return something if we actually have citations
+            return list(Counter(cits).items())
+
+    def get_law_citations(self, citations):
+        cits = []
+        for citation in citations['laws']:
+            cit = citation['text']
+            cit = ' '.join(cit.split())  # remove multiple whitespaces inside
+            try:
+                law_cit = LawCitation(cit)
+            except ValueError as ve:
+                self.logger.warning(ve)
+                continue
+            # only actually include citations that we can find in our corpus
+            if law_cit.abbreviation in self.available_laws:
+                cits.append(law_cit)
+        if cits:  # only return something if we actually have citations
+            return list(Counter(cits).items())
+
+    @staticmethod
+    def compute_relevance_score(citation_tuple):
+        """
+        Computes a relevance score for the citation between 1 and 1
+            frequency: 70%
+            year: 30%
+        :return:
+        # Proxy für relevanz score:
+        #   - häufigkeit des Zitats in Erwägungen
+        #   - neuere Urteile sind relevanter (ältere Urteile sind evtl. überholt oder nicht mehr relevant)
+        #   Thomas schreibt mir seine Ideen bald
+        #   - BGE wichtiger als andere ==> nur BGEs nehmen
+        """
+        weights = {"frequency": 0.7, "year": 0.3}
+        assert sum(weights.values()) == 1
+
+        values = {"frequency": citation_tuple[1] / 10}
+        citation = citation_tuple[0]
+        if isinstance(citation, RulingCitation):
+            # 150 corresponds to the year 2024 which should give use enough leeway
+            values["year"] = citation.year / 150
+        else:
+            values["year"] = 0.5  # set it to middle importance for all law citations
+
+        # compute weighted sum
+        score = 0
+        for key in weights.keys():
+            score += weights[key] * values[key]
+
+        return round(min(score, 1), 2)  # cap it at 1 and round to 2 decimals
 
     def get_most_common_citations(self, df, folder, type, plot_n_most_common=10):
         """
