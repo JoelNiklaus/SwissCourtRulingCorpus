93aacd83a69bb43c768940635cd0be5e908ec71e
ninabaumgartner <nina.baumgartner@students.unibe.ch>
2022-03-11 14:07:11 +0000

    Updating Sanbox

--- prodigy/run.sh
+++ prodigy/run.sh
@@ -6,15 +6,14 @@ NC="\033[0m"
 # define usage help
 usage=$(cat <<- EOF
   Arguments:
-    - task:        type of task (judgment-outcome extraction, TBD)
+    - task:        type of task (facts-annotation)
     - language:    the language to use for the server, e.g. 'de'
-    - spider_name: the name of the spider to use, e.g. 'BGer'
 
   Usage:
-    ./run.sh task language spider_name
+    ./run.sh task language
 
   Example:
-    ./run.sh judgment-outcome de BGer
+    ./run.sh facts-annotation de
 
   Consult /prodigy/README.md for more information.
 EOF
@@ -23,18 +22,17 @@ EOF
 # parse options
 task=$1
 language=$2
-spider_name=$3
 
-if [ -z "$task" ] || [ -z "$language" ] || [ -z "$spider_name" ]; then
+if [ -z "$task" ] || [ -z "$language" ] ; then
   printf "${WARN}Invalid arguments given.\n\n${NC}${usage}\n"
   exit 1
 fi
 
-if [ "$(docker ps -q -f name=prodigy_v1)" ]; then
+if [ "$(docker ps -q -f name=prodigy_v1_nina)" ]; then
   case "$task" in
-    "judgment-outcome")
+    "facts-annotation")
       printf "${SUCCESS}Starting command in container, to stop use Ctrl+C.\n${NC}"
-      docker exec -it prodigy_v1 prodigy "$task" "$spider_name" "$language" -F ./recipes/judgment_outcome.py
+      docker exec -it prodigy_v1_nina prodigy "$task" "$language" -F ./recipes/facts_annotation.py
       ;;
     *)
       printf "${WARN}Unknown task '$task' given.\n\n${NC}${usage}\n"
@@ -42,5 +40,5 @@ if [ "$(docker ps -q -f name=prodigy_v1)" ]; then
       ;;
   esac
 else
-  printf "${WARN}No container with the name prodigy_v1 found.\n Use 'bash setup.sh to start it.'${NC}"
+  printf "${WARN}No container with the name prodigy_v1_nina found.\n Use 'bash setup.sh to start it.'${NC}"
 fi
