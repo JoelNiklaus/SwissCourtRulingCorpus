d84f81dc0cca0488abbebb522dea7a291142ec65
Marco Buchholz <marco.buchholz@inf.unibe.ch>
2022-04-06 17:46:57 +0000

    pattern extractor updated for new db selection

--- scrc/preprocessors/extractors/pattern_extractor.py
+++ scrc/preprocessors/extractors/pattern_extractor.py
@@ -1,6 +1,6 @@
 
 from __future__ import annotations
-from typing import TYPE_CHECKING, Union
+from typing import Any, List, Optional, Set, TYPE_CHECKING, Tuple, Union
 from pathlib import Path
 import pandas as pd
 import sys
@@ -17,6 +17,7 @@ from scrc.enums.language import Language
 from scrc.preprocessors.extractors.abstract_extractor import AbstractExtractor
 from scrc.utils.log_utils import get_logger
 from scrc.utils.main_utils import get_config
+from scrc.utils.sql_select_utils import delete_stmt_decisions_with_df, join_decision_and_language_on_parameter, where_decisionid_in_list, where_string_spider
 
 if TYPE_CHECKING:
     from sqlalchemy.engine.base import Engine
@@ -35,9 +36,9 @@ class PatternExtractor(AbstractExtractor):
     The applying_regex step will go through all the patterns which have been found, apply self.variations and look for potential matches.
 
     recommended: python -m scrc.preprocessors.extractors.pattern_extractor 0 0
-    
+
     if you don't care about runtime:
-        
+
     python -m scrc.preprocessors.extractors.pattern_extractor 0 1
     """
 
@@ -46,13 +47,17 @@ class PatternExtractor(AbstractExtractor):
         self.logger = get_logger(__name__)
         self.variations = [(r'^', r'\s\d\d$'),
                            (r'^', r'.$'), (r'^', r'\s$'), (r'^', r'\s.$')]
+        self.columns = ['keyword', 'totalcount', 'example']
+        self.test = Language.DE
         self.df = pd.DataFrame(columns=self.columns)
         self.language = {}
+        self.currentLanguage = ''
         self.total = 0
         self.counter = 0
         self.spider = ''
         self.limit = 0
-        self.dict = {}
+        self.dict = {Language.DE: {'count': 0, 'dict': {}}, Language.FR: {
+            'count': 0, 'dict': {}}, Language.IT: {'count': 0, 'dict': {}}}
         self.end = {}
         self.logger_info = {
             'start': 'Started pattern extraction',
@@ -73,9 +78,9 @@ class PatternExtractor(AbstractExtractor):
             return pdf_raw
         return None
 
-    def get_database_selection_string(self, spider: str, lang: str) -> str:
-        """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
-        return f"spider='{spider}'"
+    # def get_database_selection_string(self, spider: str, lang: str) -> str:
+    #     """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
+    #     return f"spider='{spider}'"
 
     def add_columns(self, engine: Engine):
         return None
@@ -92,62 +97,94 @@ class PatternExtractor(AbstractExtractor):
 
     def process_one_df_row(self, series: pd.DataFrame) -> pd.DataFrame:
         """Processes one row of a raw df"""
-        namespace = series[['date', 'html_url', 'pdf_url', 'id']].to_dict()
+        # namespace = series[['date', 'html_url', 'pdf_url', 'id']].to_dict()
+        # namespace['language'] = Language(series['language'])
+        namespace = dict()
+        if 'html_url' in series:
+            namespace['html_url'] = series.get('html_url')
         namespace['language'] = Language(series['language'])
+        namespace['id'] = series['decision_id']
         data = self.get_required_data(series)
         assert data
         self.analyze_structure(self.call_processing_function(
             series["spider"], data, namespace
         ), namespace)
 
-    def process_one_spider(self, engine: Engine, spider: str):
+    def save_data_to_database(self, series: pd.DataFrame, engine: Engine):
+        """Splits the data into their respective parts and saves them to the table"""
+
+    def start_spider_loop(self, spider_list: Set, engine: Engine):
+        for spider in spider_list:
+            self.process_one_spider(engine, spider)
+        self.mark_as_processed(self.processed_file_path, spider)
+
+    def select_df(self, engine: str, spider: str) -> str:
+        """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
         self.spider = spider
-        for lang in self.languages:
-            self.counter = 0
-            self.logger.info(
-                self.logger_info["start_spider"] + " " + spider + " " + lang)
-            where = self.get_database_selection_string(spider, lang)
-            self.start_progress(engine, spider, lang)
-            dfs = self.select(engine, lang, where=where,
-                              chunksize=self.chunksize)
-            for df in dfs:
-                if self.counter >= self.total_to_process:
-                    break
-                self.total = self.total_to_process
-                df.apply(self.process_one_df_row, axis="columns")
-            self.create_dfs(Language[lang.upper()])
+        only_given_decision_ids_string = f" AND {where_decisionid_in_list(self.decision_ids)}" if self.decision_ids is not None else ""
+        return self.select(engine, f"file {join_decision_and_language_on_parameter('file_id', 'file.file_id')}", f"decision_id, iso_code as language, html_raw, pdf_raw, '{spider}' as spider", where=f"file.file_id IN {where_string_spider('file_id', spider)} {only_given_decision_ids_string}", chunksize=self.chunksize)
+
+    def process_one_spider(self, engine: Engine, spider: str):
+        self.logger.info(self.logger_info["start_spider"] + " " + spider)
+
+        dfs = self.select_df(engine, spider)
+        # TODO make quick request to see if there are decisions at all: if not, skip lang so no confusing report is printed
+        #self.start_progress(engine, spider)
+        # stream dfs from the db
+        #dfs = self.select(engine, lang, where=where, chunksize=self.chunksize)
+        for df in dfs:
+            df = df.apply(self.process_one_df_row, axis="columns")
+            # self.update(engine, df, lang, [self.col_name], self.output_dir)
+            # self.log_progress(self.chunksize)
+
+        #self.log_coverage(engine, spider)
+        self.dict[self.currentLanguage]['count'] = self.counter
+        self.create_dfs()
+        self.logger.info(f"{self.logger_info['finish_spider']} {spider}")
 
     def analyze_structure(self, paragraphs: list, namespace: dict):
-        url = namespace['pdf_url']
-        if url == '':
-            url = namespace['html_url']
+        self.count_total_cases(namespace)
+        # no more url in namespace?
+        # url = namespace['pdf_url']
+        # if url == '':
+        #     url = namespace['html_url']
         self.counter += 1
         noDuplicates = list(dict.fromkeys(paragraphs))
         for item in noDuplicates:
             if item is not None and (4 < len(item) < 80):
-                self.check_existance(
-                    item, url)
+                self.check_existance(item, namespace)
         if self.counter % 500 == 0:
-            self.logger.info(self.get_progress_string(
-                self.counter, self.total_to_process, "Pattern extraction"))
+            self.logger.info(
+                f"Pattern extractor: {self.spider}: {self.counter} processed")
 
-    def create_dfs(self, lang):
-        if len(self.dict) > 0:
-            self.end[lang] = pd.DataFrame.from_dict(self.dict, orient='index')
-            self.end[lang] = self.drop_rows(self.end[lang])
-            if sys.argv[2] == "1":
-                self.logger.info(
-                    f"Finished pattern extraction, applying regex now")
-                self.end[lang] = self.apply_regex(self.end[lang])
-            self.assign_section(self.end[lang], {"language": lang})
-            self.dict = {}
+    def create_dfs(self):
+        for key in self.dict:
+            if self.dict[key]['count'] > 0:
+                self.end[key] = pd.DataFrame.from_dict(
+                    self.dict[key]['dict'], orient='index')
+                self.end[key] = self.drop_rows(self.end[key])
+                # needs to get worked over
+                if sys.argv[1] == "1":
+                    self.logger.info(
+                        f"Finished pattern extraction, applying regex now")
+                    self.end[key] = self.apply_regex(self.end[key])
+                self.assign_section(
+                    self.end[key], {"language": key}, self.dict[key]['count'])
 
-    def check_existance(self, item: str, url):
+    def count_total_cases(self, namespace: dict):
+        if self.currentLanguage != namespace['language']:
+            if self.currentLanguage != "":
+                self.dict[self.currentLanguage]['count'] = self.counter
+            self.currentLanguage = namespace['language']
+            self.counter = 0
+
+    def check_existance(self, item: str, namespace):
         item = re.sub('^=+', '', item)
-        if item in self.dict:
-            self.dict[item]['totalcount'] += 1
+        if item in self.dict[namespace['language']]['dict']:
+            self.dict[namespace['language']]['dict'][item]['totalcount'] += 1
         else:
-            self.dict[item] = {"totalcount": 1, "url": url, "keyword": item}
+            self.dict[namespace['language']]['dict'][item] = {
+                "totalcount": 1, "keyword": item}
 
     def merge_rows(self, indices, df: DataFrame):
         for index in indices[1:]:
@@ -167,13 +204,13 @@ class PatternExtractor(AbstractExtractor):
         startingIndex = 0
         while(condition):
             sliced = s.iloc[startingIndex:]
-            if startingIndex % 500 == 0:
-                self.logger.info(self.get_progress_string(
-                    startingIndex, s.size, "Regex application"))
             if len(sliced) == 0:
                 return df
             for index, value in sliced.items():
                 startingIndex += 1
+                if startingIndex % 500 == 0:
+                    self.logger.info(self.get_progress_string(
+                        startingIndex, s.size, "Regex application"))
                 foundInstance = False
                 for idx, element in enumerate(self.variations):
                     pattern = self.get_pattern(value, element)
@@ -198,7 +235,7 @@ class PatternExtractor(AbstractExtractor):
             df.drop(index, inplace=True)
         return df
 
-    def assign_section(self, df: DataFrame, namespace):
+    def assign_section(self, df: DataFrame, namespace, count):
         all_section_markers = {
             Language.FR: {
                 Section.FACTS: [r'[F,f]ait', 'droit'],
@@ -231,6 +268,8 @@ class PatternExtractor(AbstractExtractor):
                 key = 'unknown'
             dfs[key] = pd.DataFrame([], columns=df.columns)
         df.reset_index(inplace=True)
+        if 'keyword' not in df.columns:
+            print(df)
         for index, element in enumerate(df['keyword'].tolist()):
             if index % 100 == 0:
                 self.logger.info(self.get_progress_string(
@@ -239,40 +278,35 @@ class PatternExtractor(AbstractExtractor):
             for key in section_markers:
                 if re.search(section_markers[key], element):
                     if key == Section.RULINGS or len(element) < 45:
+                        row = df.loc[index]
+                        row['coverage'] = row['totalcount'] / count * 100
                         dfs[key] = dfs[key].append(
-                            df.loc[index], ignore_index=True)
+                            row, ignore_index=True)
                         foundAssigntment = True
             if not foundAssigntment:
+                row = df.loc[index]
+                row['coverage'] = row['totalcount'] / count * 100
                 dfs['unknown'] = dfs['unknown'].append(
-                    df.loc[index], ignore_index=True)
-        self.df_to_csv(self.calculate_coverage(dfs), namespace['language'])
+                    row, ignore_index=True)
+        self.df_to_csv(dfs, namespace['language'])
 
     def get_progress_string(self, progress: int, total: int, name: str):
         return f"{name}: {progress} of {total} processed"
 
-    def calculate_coverage(self, dfs):
-        for key in dfs:
-            if key == 'unknown':
-                dfs[key] = self.drop_rows(dfs[key])
-            dfs[key]['coverage'] = 0
-            dfs[key]['coverage'] = dfs[key].apply(self.get_percentage, axis=1)
-            if 'index' in dfs[key].columns:
-                dfs[key].drop(columns=['index'], inplace=True)
-        return dfs
-
-    def get_percentage(self, s):
-        return s.at['totalcount'] / self.total * 100
-
     def df_to_csv(self, dfs, lang):
         with pd.ExcelWriter(self.get_path(self.spider, lang)) as writer:
             for key in dfs:
-                dfs[key].sort_values(
-                    by=['totalcount'], ascending=False).to_excel(writer, sheet_name=str(key), index=False)
+                if key != Section.FULLTEXT:
+                    dfs[key].sort_values(
+                        by=['totalcount'], ascending=False).to_excel(writer, sheet_name=str(key), index=False)
 
     def drop_rows(self, df: DataFrame):
-        df.drop(
-            df[df.totalcount < 2].index, inplace=True)
-        df.reset_index(drop=True, inplace=True)
+        if 'totalcount' in df.columns:
+            df.drop(
+                df[df.totalcount < 2].index, inplace=True)
+            df.reset_index(drop=True, inplace=True)
+        else:
+            print(df)
         return df
 
     def get_path(self, spider: str, lang):
@@ -284,5 +318,5 @@ class PatternExtractor(AbstractExtractor):
 
 if __name__ == '__main__':
     config = get_config()
-    section_splitter = PatternExtractor(config)
-    section_splitter.start()
+    pattern_extractor = PatternExtractor(config)
+    pattern_extractor.start()
