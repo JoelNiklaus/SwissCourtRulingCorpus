4ec28afbb30fddbbf297b354671373d3fe21d12a
ninabaumgartner <nina.baumgartner@students.unibe.ch>
2022-04-07 09:01:38 +0000

    Adapted .gitignore. Changed docker set up. Changed dataset_creation.py and recipe.

--- prodigy/setup.sh
+++ prodigy/setup.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+WARN="\033[1;31m"
+SUCCESS="\033[1;32m"
+NC="\033[0m"
+
+# fetch env variables
+if test -f .env; then
+    source .env
+else
+    echo "No .env file found. Please create one containing DB_USERNAME and DB_PASSWORD."
+    exit 1
+fi
+echo "Stopping and removing container prodigy_v1_nina "
+docker stop prodigy_v1_nina
+docker rm prodigy_v1_nina
+docker image rm prodigy_v1_nina:latest
+echo "Building image"
+docker build \
+  --build-arg user=${PRODIGY_BASIC_AUTH_USER} \
+  --build-arg password=${PRODIGY_BASIC_AUTH_PASS} \
+  -t prodigy_v1_nina .
+
 echo "Setup started..."
 
 WARN="\033[1;31m"
