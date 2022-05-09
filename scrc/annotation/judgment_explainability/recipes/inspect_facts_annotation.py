2cad3130a070d13faef85a77d7a2d26b019b1b6f
ninabaumgartner <nina.baumgartner@students.unibe.ch>
2022-05-09 09:38:33 +0000

    Added new recipes and commands to run.sh, added new recipe inspect_facts_annotation

---
+++ scrc/annotation/judgment_explainability/recipes/inspect_facts_annotation.py
@@ -0,0 +1,56 @@
+#  prodigy review-facts-annotation de nina -F ./recipes/review_facts_annotation.py
+
+import sqlite3
+import spacy
+import json
+import prodigy
+from prodigy.components.loaders import JSONL
+from prodigy.util import split_string
+from prodigy.components.preprocess import add_tokens
+import psycopg2
+from psycopg2 import sql
+from typing import List, Optional
+import os
+from prodigy.components.db import connect
+ports ={"angela":11001, "lynn":11002, "thomas":11003,"fr": 12001, "it": 13001}
+
+# helper function for recipe command line input
+def split_string(string):
+  return string.split(",")
+
+# the recipe itself, for info on how to use it, see the prodigy docs
+@prodigy.recipe(
+  "inspect-facts-annotation",
+    language=("The language to use for the recipe. Type test for test mode", 'positional', None, str),
+    annotator=("The annotator who marking the cases.", 'positional', None, str),
+)
+
+# function called by the @prodigy-recipe definition
+def inspect_facts_annotation(language:str,annotator:str ):
+  # define labels for annotation
+  labels = ["Supports judgment","Opposes judgment","Lower court"]
+  stream = JSONL("./annotations/annotations_{}-{}.jsonl".format(language,annotator))
+
+
+  dataset = "annotations_{}_review".format(language)
+  port = ports[annotator]
+
+  return {
+    "dataset": dataset ,# Name of dataset_scrc to save annotations
+    "view_id": "blocks",
+    "stream": stream,
+    "config": {
+      "port": port,
+      "blocks": [
+        {"view_id": "html",
+         "html_template": "<p style='float:left'>{{file_number}}</p>"},
+        {"view_id": "html", "html_template": "<h1 style='float:left'>{{header}} – Judgment: {{judgment}}</h2>"},
+        {"view_id": "html",
+         "html_template": "<h2 style='float:left'>Facts</h2><a style='float:right' href='{{link}}' target='_blank'>Go to the court ruling</a>"},
+        {"view_id": "spans_manual", "labels": labels},
+        {"view_id": "text_input","field_label":"Annotator comment on this ruling", "field_placeholder": "Type here...","field_rows": 5},
+      ]
+    },
+
+    }
+
--- scrc/annotation/judgment_explainability/run.sh
+++ scrc/annotation/judgment_explainability/run.sh
@@ -9,7 +9,7 @@ NC="\033[0m"
 # define usage help
 usage=$(cat <<- EOF
   Arguments:
-    - task:        type of task (e.g. facts-annotation)
+    - task:        type of task (facts-annotation, inspect-facts-annotation)
   Usage:
     -bash run.sh task
   Example:
@@ -33,8 +33,35 @@ if [ "$(docker ps -q -f name=prodigy_v1_nina)" ]; then
       docker exec -it -d prodigy_v1_nina prodigy "$task" de -F ./recipes/facts_annotation.py
       docker exec -it -d prodigy_v1_nina prodigy "$task" fr -F ./recipes/facts_annotation.py
       docker exec -it -d prodigy_v1_nina prodigy "$task" it -F ./recipes/facts_annotation.py
-      printf "${SUCCESS}Starting command in container, to stop use Ctrl+C.\n${NC}"
+      printf "${SUCCESS}Starting $task command in container, to stop use Ctrl+C.\n${NC}"
       ;;
+
+
+    "inspect-facts-annotation")
+    docker exec -it -d prodigy_v1_nina prodigy "$task" de angela -F ./recipes/inspect_facts_annotation.py
+    docker exec -it -d prodigy_v1_nina prodigy "$task" de lynn -F ./recipes/inspect_facts_annotation.py
+    docker exec -it -d prodigy_v1_nina prodigy "$task" de thomas -F ./recipes/inspect_facts_annotation.py
+    printf "${SUCCESS}Starting $task command in container, to stop use Ctrl+C.\n${NC}"
+    ;;
+
+  "review")
+  docker exec -it prodigy_v1_nina prodigy "$task" gold_annotations_de annotations_de-angela,annotations_de-lynn,annotations_de-thomas -l "Supports judgment","Opposes judgment","Lower court" -v spans_manual --auto-accept
+  ;;
+  "db-out")
+   docker exec prodigy_v1_nina prodigy "$task" annotations_de > ./annotations/annotations_de.jsonl
+   docker exec prodigy_v1_nina prodigy "$task" annotations_de-angela > ./annotations/annotations_de-angela.jsonl
+   docker exec prodigy_v1_nina prodigy "$task" annotations_de-lynn > ./annotations/annotations_de-lynn.jsonl
+   docker exec prodigy_v1_nina prodigy "$task" annotations_de-thomas > ./annotations/annotations_de-thomas.jsonl
+    ;;
+
+  "stats")
+  docker exec prodigy_v1_nina prodigy "$task" annotations_de-angela
+  docker exec prodigy_v1_nina prodigy "$task" annotations_de-lynn
+  docker exec prodigy_v1_nina prodigy "$task" annotations_de-thomas
+  ;;
+
+
+
     *)
       printf "${WARN}Unknown task '$task' given.\n\n${NC}${usage}\n"
       exit 1
