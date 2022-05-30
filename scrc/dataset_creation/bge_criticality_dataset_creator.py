f387cc9e38544eda8480351741938f8acb2dc7e0
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2022-05-30 11:00:47 +0200

    rename to bge_criticality

--- scrc/dataset_creation/bge_criticality_dataset_creator.py
+++ scrc/dataset_creation/bge_criticality_dataset_creator.py
@@ -1,98 +1,148 @@
 from scrc.dataset_creation.dataset_creator import DatasetCreator
-from root import ROOT_DIR
-from pathlib import Path
-from scrc.utils.main_utils import get_config
 from scrc.utils.log_utils import get_logger
-import numpy as np
-from tqdm import tqdm
-from pandarallel import pandarallel
+import pandas as pd
+
+from scrc.utils.main_utils import get_config
 
 """
-Dataset to be created:
-- contains supreme court cases  
-- cols = feature_col and label
-- only cases where feature_col text has good length
-- Dataset description:
-    - train.jsonl:
-        contains only the train split of queries.jsonl (years 2000 - 2014)
-    - val.jsonl:
-        contains only the validation split of queries.jsonl (years 2015 - 2016)
-    - test.jsonl:
-        contains only the test split of queries.jsonl (years 2017 - 2021)
+Datasets to be created:
+- BGE
+    Contains all BGE since ? 
+    cols = language, canton, date, file-number, text
+- BGer
+    contains all bger since 
+    cols = language, canton, date, file-number, text
 Set Labels
     - criticality based on BGE
-        - get all bger whose file numbers were extracted by bge_reference_extractor
+        - get all bger whose file numbers appear in a bge header
+        - use of "bge_reference_extractor"
         - set label critical for those found bger
-Check distribution of data sets
-    - distribution among languages
-    - distribution among legal areas
-    - distribution among cantons
-    - is there bias detectable?
+    - criticality based on ruling citations
+        - set label according number of citations found for a bge
+        - critical if score more than ??? citations are found
+        - distinct between links that are 10 years old and those which are newer
+        - set of critical cases using citations is subset of criticality based on BGE
+    - criticality based on published "Medienmitteilungen"
+        - 
 """
+
 
 class BgeCriticalityDatasetCreator(DatasetCreator):
     """
-    Defines a criticality label for each found bger case, based if the case was published as
-    bge or not.
+    Creates a dataset with the text as input and whether it reaches the supreme court or not as labels
     """
+
+    # TODO filter out cases where facts or other input for training model is too short
+    # - what is used as input?
+    # - what's the input length?
+    # TODO filter only supreme court cases
+    # - are there any constraints? time, legal area, ...
+    # TODO Criticality definition 1
+    # - Filter all cases that were published with abbreviation BGE
+    # - Check if one can find matching bger cases for all BGE cases
+    # - make sure no case is found twice
+    # - define all BGE cases as criticalBGE
+    # TODO Criticality definition 2
+    # - get data set of newspaper occurrences
+    # - check if one needs to filter certain cases or if all can be used
+    # - get case for a occurrence in newspaper
+    # - define all cases with occurrence as criticalNEWS
+    # TODO Criticality definition 3
+    # - get data set of links / references
+    # - define all cases that were referenced to as criticalLINK
+    # TODO Check distribution of data sets
+    # - distribution among languages
+    # - distribution among legal areas
+    # - distribution among cantons
+    # - is there bias detectable?
 
     def __init__(self, config: dict):
         super().__init__(config)
         self.logger = get_logger(__name__)
-        self.debug = True
+
+        self.debug = False
         self.split_type = "date-stratified"
-        # Todo check if names for each criticality creator should be unique
         self.dataset_name = "criticality_prediction"
         self.feature_cols = ['text']  # ['facts', 'considerations', 'text']
 
-        # TODO what is this code doing?
-        pandarallel.initialize(progress_bar=True)
-        tqdm.pandas()
+        # self.with_partials = False
+        # self.with_write_off = False
+        # self.with_unification = False
+        # self.with_inadmissible = False
+        # self.make_single_label = True
 
     def get_dataset(self, feature_col, lang, save_reports):
-        """get all bger cases and set labels"""
+        # create engine
+        engine = self.get_engine(self.db_scrc)
+        # get bge rulings
+        bge_df = self.query_bge(feature_col, engine, lang)
+        # get bger rulings
+        bger_df = self.query_bger(feature_col, engine, lang)
+        # set criticality label
+        bger_criticality_df = self.query_bge_criticality(bger_df, bge_df)
+        labels = ['non-critical', 'critical']
+        return bger_criticality_df, labels
 
-        df = self.get_df(self.get_engine(self.db_scrc), feature_col, 'citations', lang, save_reports)
-        df = self.set_bge_criticality_label(df)
-
-        # TODO need to drop something else?
-        df = df.drop(['citations', 'counter', 'rulings'], axis=1)
-        # TODO rename neccessarry?
-        # df = df.rename(columns={feature_col: "text"})  # normalize column names
-        labels, _ = list(np.unique(np.hstack(df.label), return_index=True))
-        return df, labels
-
-    def set_bge_criticality_label(self, df):
-        """set for each bger ruling a label critical or non-critical depending on whether their
-        file number was extracted in a bge"""
-        # Include all bger rulings whose file_number can be found in the header of a bge
-        # error sources:
-        # 1. Regex cannot find correct file number in header
+    # set criticality labels
+    def query_bge_criticality(self, bger_df, bge_df):
         self.logger.info(f"Processing labeling of bge_criticality")
 
-        bge_references_file_path: Path = ROOT_DIR / 'data' / 'progress' / "bge_references_found.txt"
-        if not bge_references_file_path.exists():
-            raise Exception("bge references need to be extracted first. Run bge_reference_extractor.")
-        bge_references = bge_references_file_path.read_text().strip().split("\n")
-        file_number_match = df.file_number.astype(str).isin(list(bge_references))
-        critical_df = df[file_number_match]
-        critical_df['bge_label'] = 'critical'
-        non_critical_df = df[~file_number_match]
-        non_critical_df['bge_label'] = 'non-critical'
+        # Include all bger rulings whose file_number can be found in the header of a bge
+        # It's not enough no compare date and chamber, there are multiple matching cases
+        # There exist around 12'000 rulings with date = 1.1.2020
+        # error sources:
+        # 1. Regex cannot find correct file number in header
+        # 2. languages are different -> different datasets
+
+        # TODO create method comparing bger file numbers to found regex expression in bge
+        """
+        file_number_match = bger_df.file_number.astype(str).isin(list(bge_df.bge_reference.astype(str)))
+        file_number_match_df = bger_df[file_number_match]       
+        critical_df = bger_df[file_number_match]
+        critical_df['label'] = 'critical'
+        non_critical_df = bger_df[~file_number_match]
+        non_critical_df['label'] = 'non-critical'
         self.logger.info(f"# critical decisions: {len(critical_df.index)}")
         self.logger.info(f"# non-critical decisions: {len(non_critical_df.index)}")
-        self.calculate_label_coverage(bge_references, file_number_match, critical_df, bger_df)
         return critical_df.append(non_critical_df)
+        """
+        return bger_df
 
-    def calculate_label_coverage(self, bge_references, file_number_match, critical_df, bger_df):
-        """Calculate some numbers on how many cases could be labeled correctly and hwo many are still missing"""
-        self.logger.info(f"there were {len(bge_references)} references extracted")
-        bge_references = set(bge_references)
-        self.logger.info(f"{len(bge_references)} of the entries were unique")
-        # get references which were extracted but not found in bger cases
-        extracted_and_found = list(critical_df.file_number.astype(str))
-        new_list = [decision for decision in bge_references if decision not in extracted_and_found]
-        self.logger.info(f"{len(new_list)} references were extracted but not found")
+    # get all bger
+    def query_bger(self, feature_col, engine, lang):
+        # TODO which columns are needed
+        columns = ['id', 'chamber', 'date', 'extract(year from date) as year', f'{feature_col}', 'file_name', 'file_number']
+        try:
+            bger_df = next(self.select(engine, lang,
+                                      columns=",".join(columns),
+                                      where="court = 'CH_BGer'",
+                                      order_by="date",
+                                      chunksize=self.get_chunksize()))
+        except StopIteration:
+            raise ValueError("No bger rulings found")
+        # get rid of all dublicated cases
+        # TODO improve this
+        bger_df = bger_df.dropna(subset=['date', 'id'])
+        self.logger.info(f"Found {len(bger_df.index)} supreme bger rulings")
+        return bger_df
+
+    # get all bge
+    def query_bge(self, feature_col, engine, lang):
+        # TODO which columns are needed
+        columns = ['id', 'chamber', 'date', 'extract(year from date) as year', f'{feature_col}', 'file_name', 'file_number']
+        try:
+            bge_df = next(self.select(engine, lang,
+                                                columns=",".join(columns),
+                                                where="spider = 'CH_BGE'",
+                                                order_by="date",
+                                                chunksize=self.get_chunksize()))
+        except StopIteration:
+            raise ValueError("No bge rulings found")
+        # get rid of all dublicated cases
+        # TODO improve this
+        bge_df = bge_df.dropna(subset=['date', 'id'])
+        self.logger.info(f"Found {len(bge_df.index)} supreme bge rulings")
+        return bge_df
 
 
 if __name__ == '__main__':
@@ -100,4 +150,3 @@ if __name__ == '__main__':
 
     bge_criticality_dataset_creator = BgeCriticalityDatasetCreator(config)
     bge_criticality_dataset_creator.create_dataset()
-
