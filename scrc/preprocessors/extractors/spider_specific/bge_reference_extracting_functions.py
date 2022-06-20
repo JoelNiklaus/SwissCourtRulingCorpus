5b06aa876285c4150cfa446477b5e5a046b3472c
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2022-06-20 21:34:01 +0200

    correct pattern recognition for bge_references, replace _ and . with a blank space

--- scrc/preprocessors/extractors/spider_specific/bge_reference_extracting_functions.py
+++ scrc/preprocessors/extractors/spider_specific/bge_reference_extracting_functions.py
@@ -26,14 +26,28 @@ def CH_BGE(sections: Dict[Section, str], namespace: dict) -> Optional[str]:
     """
 
     header = sections[Section.HEADER]
-    citation_regexes = json.loads((ROOT_DIR / 'legal_info' / 'bge_origin_bger_reference_citation.json').read_text())
 
+    # TODO delete code and delete json file
+    """
+    citation_regexes = json.loads((ROOT_DIR / 'legal_info' / 'bge_origin_bger_reference_citation.json').read_text())
     def get_combined_regexes(regex_dict):
         return re.compile("|".join([entry["regex"] for entry in regex_dict if entry["regex"]]))
-
     bge_reference_pattern = get_combined_regexes(citation_regexes['ruling']['bge_reference'])
-
     bge_reference = re.search(bge_reference_pattern, header)
+        return str(bge_reference)
+    """
 
+    # create pattern for bge references: 1_456/2021 oder 1a.456/2021 oder 3a 32/2019
+    pattern = re.compile('\d\D?_\d{1,3}/\d{4}|\d\D?\.\d{1,3}/\d{4}|\d\D?\s\d{1,3}/\d{4}')
+    bge_reference = pattern.findall(header)[0]
+    # in database decisions are saved with space, make sure pattern is the same
+    bge_reference = bge_reference.replace('_', ' ')
+    bge_reference = bge_reference.replace('.', ' ')
     return str(bge_reference)
+
+
+
+
+
+
 
