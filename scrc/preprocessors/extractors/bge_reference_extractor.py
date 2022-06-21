ac98060aed4e35d827dae7c211e8be8927445f88
Stern5497 <62178719+Stern5497@users.noreply.github.com>
2022-06-21 15:36:54 +0200

    improve writing bge refernces into file, not overwriting it. exclude cases for which no reference could be found

--- scrc/preprocessors/extractors/bge_reference_extractor.py
+++ scrc/preprocessors/extractors/bge_reference_extractor.py
@@ -45,33 +45,21 @@ class BgeReferenceExtractor(AbstractExtractor):
 
     def select_df(self, engine: str, spider: str) -> str:
         """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
-        """only_given_decision_ids_string = f" AND {where_decisionid_in_list(self.decision_ids)}" if self.decision_ids is not None else ""
+        only_given_decision_ids_string = f" AND {where_decisionid_in_list(self.decision_ids)}" if self.decision_ids is not None else ""
         # TODO test what is selected
         # select analog zur Abfrage in court_composition_extractor aber ohne footer
         to_return = self.select(engine, f"section headersection {join_decision_and_language_on_parameter('decision_id', 'headersection.decision_id')} {join_file_on_decision()}", f"headersection.decision_id, headersection.section_text as header, '{spider}' as spider, iso_code as language, html_url", where=f"headersection.section_type_id = 1 AND headersection.decision_id IN {where_string_spider('decision_id', spider)} {only_given_decision_ids_string}", chunksize=self.chunksize)
         return to_return
-        """
-        """Returns the `where` clause of the select statement for the entries to be processed by extractor"""
-        only_given_decision_ids_string = f" AND {where_decisionid_in_list(self.decision_ids)}" if self.decision_ids is not None else ""
-        # Joining the footer on to the same row as the header so each decision goes through the erxtractor only once per decision
-        section_self_join = 'LEFT JOIN section footersection ON headersection.decision_id = footersection.decision_id ' \
-                            'AND footersection.section_type_id = 6'
-        decision_language_join = join_decision_and_language_on_parameter('decision_id', 'headersection.decision_id')
-        tables = f"section headersection {decision_language_join} {join_file_on_decision()} {section_self_join}"
-        columns = f"headersection.decision_id, headersection.section_text as header, " \
-                  f"footersection.section_text as footer,'{spider}' as spider, iso_code as language, html_url"
-        where = f"headersection.section_type_id = 1 " \
-                f"AND headersection.decision_id IN {where_string_spider('decision_id', spider)} {only_given_decision_ids_string}"
-        return self.select(engine, tables, columns, where=where, chunksize=self.chunksize)
+
 
     def save_data_to_database(self, df: pd.DataFrame, engine: Engine):
         self.logger.info("save data in progress")
         processed_file_path = ROOT_DIR / 'data' / 'progress' / "bge_references_found.txt"
         for _, row in df.iterrows():
-            with processed_file_path.open("w+") as f:
-                if 'bge_reference' in row or row['bge_reference'] is not None:
+            with processed_file_path.open("a") as f:
+                if 'bge_reference' in row and row['bge_reference'] != 'no reference found':
                     bge_reference = str(row['bge_reference'])
-                    f.write(bge_reference + "\n")
+                    f.write(f"{bge_reference}\n")
 
 
     def check_condition_before_process(self, spider: str, data: Any, namespace: dict) -> bool:
@@ -84,3 +72,4 @@ if __name__ == '__main__':
     config = get_config()
     bge_reference_extractor = BgeReferenceExtractor(config)
     bge_reference_extractor.start()
+    # delete bge_reference_found.txt file before letting it run.
