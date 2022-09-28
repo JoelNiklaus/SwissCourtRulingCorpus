1fa78fafea374ec47a75a6b3ad6a49862ff7e7b1
Marco Buchholz <marco.buchholz@inf.unibe.ch>
2022-09-28 12:47:50 +0000

    removed unused class methods

--- scrc/enums/section.py
+++ scrc/enums/section.py
@@ -11,11 +11,5 @@ class Section(Enum):
     FOOTER = 7
 
 
-    @classmethod
-    def without_facts(cls):
-        return cls.HEADER, cls.HEADER,cls.CONSIDERATIONS, cls.RULINGS, cls.FOOTER
-    
-    @classmethod
-    def without_topic(cls):
-        return cls.FULLTEXT, cls.HEADER, cls.FACTS, cls.CONSIDERATIONS, cls.RULINGS, cls.FOOTER
+
     
